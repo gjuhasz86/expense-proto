9a9d237767cc1859a5ea4d259684bb293539ce20
Gabor Juhasz <jaysicks.reg@gmail.com>
2016-05-29 00:19:41 +0200

    added openshift install script

---
+++ .scripts/openshift-create.sh.bat
@@ -0,0 +1,36 @@
+echo -n "Git repo (Leave empty if app is existing on openshift): "
+read gitrepo
+echo -n "App name: "
+read app
+echo -n "Database name: "
+read dbname
+echo -n "Database user name: "
+read user
+echo -n "Database user password: "
+read pass
+
+if [ -z "$gitrepo" ]; then
+  echo "Skip creating app";
+else
+  rhc app-create $app https://raw.githubusercontent.com/icflorescu/openshift-cartridge-nodejs/master/metadata/manifest.yml --from-code $gitrepo;
+  
+  # workaround for npm 3.9.3 bug - delete temporary npm dir
+  rhc ssh 'rm -rf app-root/data/.nodejs/lib/node_modules/npm/.nyc_output'
+
+  rhc cartridge add -c mongodb-2.4 --app $app  
+  
+  rhc cartridge add -c rockmongo --app $app
+
+  rhc app-restart -a $app
+fi
+
+# set environment variables that the app is using
+rhc env-set OPENSHIFT_EXPENSE_DB=$dbname -a $app
+rhc env-set OPENSHIFT_EXPENSE_USER=$user -a $app
+rhc env-set OPENSHIFT_EXPENSE_PWD=$pass -a $app
+
+# create user in the database
+addUserCmd="db.getSiblingDB('$dbname').addUser('$user', '$pass')"
+rhc ssh -a $app 'mongo $OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/admin -u $OPENSHIFT_MONGODB_DB_USERNAME -p $OPENSHIFT_MONGODB_DB_PASSWORD --eval "'$addUserCmd'"'
+
+rhc app-restart -a $app
