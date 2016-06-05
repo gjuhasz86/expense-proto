5cdb2d0caee5a6f1646984528c439bc28414f7c6
Gabor Juhasz <jaysicks.reg@gmail.com>
2016-06-05 14:28:47 +0200

    Added Google authentication

--- server/app/auth.js
+++ server/app/auth.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+var db = require('./db');
 
 var router = express.Router();
+var env = process.env;
 
 router.use(passport.initialize());
 router.use(passport.session());
@@ -17,33 +21,71 @@ passport.serializeUser(function (user, done) {
 });
 
 passport.deserializeUser(function (id, done) {
-    if (id == '31337') {
-        done(null, {id: '31337', username: 'admin'});
-    }
-    else if (id == '4006') {
-        done(null, {id: '4006', username: 'noob'});
-    }
-    else {
-        done({error: "no such user"});
-    }
+    // if (id == '31337') {
+    //     done(null, {id: '31337', username: 'admin'});
+    // }
+    // else if (id == '4006') {
+    //     done(null, {id: '4006', username: 'noob'});
+    // }
+    // else {
+    //     done({error: "no such user"});
+    // }
+    db.dbRef.collection("users").find({id: id}).limit(1).next(function (err, doc) {
+        console.log("deserialized user");
+        console.log(JSON.stringify(doc));
+        return done(null, doc);
+    })
 });
 
-passport.use('local-login', new LocalStrategy(
-    function (username, password, done) {
+passport.use(new GoogleStrategy({
+        clientID: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/callback"
+    },
+    function (accessToken, refreshToken, profile, done) {
         console.log("authenticating");
-        if (username == 'admin' && password == 'simple') {
-            return done(null, {id: '31337', username: 'admin'});
-        } else if (username == 'noob' && password == 'simple') {
-            return done(null, {id: '4006', username: 'noob'});
-        } else {
-            return done(null, false, {message: 'Incorrect username or password.'});
-        }
+        console.log(JSON.stringify(profile));
+        db.dbRef.collection("users").findOneAndUpdate(
+            {id: profile.id},
+            {$set: {id: profile.id, username: profile.displayName}},
+            {upsert: true, returnOriginal: false}, function (err, r) {
+                console.log("created user");
+                console.log(JSON.stringify(r));
+                return done(null, r.value);
+            }
+        );
+
+
     }
 ));
+
+// passport.use('local-login', new LocalStrategy(
+//     function (username, password, done) {
+//         console.log("authenticating");
+//         if (username == 'admin' && password == 'simple') {
+//             return done(null, {id: '31337', username: 'admin'});
+//         } else if (username == 'noob' && password == 'simple') {
+//             return done(null, {id: '4006', username: 'noob'});
+//         } else {
+//             return done(null, false, {message: 'Incorrect username or password.'});
+//         }
+//     }
+// ));
+
+router.get('/auth/google',
+    passport.authenticate('google', {scope: ['https://www.googleapis.com/auth/plus.login']}));
+
+router.get('/auth/google/callback',
+    passport.authenticate('google', {failureRedirect: '/login'}),
+    function (req, res) {
+        res.redirect('/');
+    });
+
 
 router.post('/auth/login', passport.authenticate('local-login'), function (req, res) {
     res.json(req.user);
 });
+
 
 router.get('/auth/logout', function (req, res) {
     console.log('logout');
