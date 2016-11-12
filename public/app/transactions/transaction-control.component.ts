f44d423ff541d4bafb64a88ebb2b5cc13a30e387
Gabor Juhasz <jaysicks.reg@gmail.com>
2016-11-12 01:07:29 +0100

    added master checkbox

--- public/app/transactions/transaction-control.component.ts
+++ public/app/transactions/transaction-control.component.ts
@@ -5,6 +5,7 @@ import {Category} from "../categories/category";
 export class TransactionControlService {
     addCategory: EventEmitter<any> = new EventEmitter();
     remCategory: EventEmitter<any> = new EventEmitter();
+    setSelection: EventEmitter<any> = new EventEmitter();
 }
 
 @Component({
@@ -14,6 +15,7 @@ export class TransactionControlService {
 export class TransactionControlComponent {
     @Output() addCategory: EventEmitter<any> = this._ctrlSvc.addCategory;
     @Output() remCategory: EventEmitter<any> = this._ctrlSvc.remCategory;
+    @Output() setSelection: EventEmitter<any> = this._ctrlSvc.setSelection;
 
     constructor(private _ctrlSvc: TransactionControlService) {
     }
@@ -28,5 +30,10 @@ export class TransactionControlComponent {
     public sendRemCategory(c: string) {
         console.log("TnxCtrl emitting: " + c);
         this.remCategory.emit(c);
+    }
+
+    public sendSetSelection(b: boolean) {
+        console.log("TnxCtrl emitting: " + b);
+        this.setSelection.emit(b);
     }
 }
