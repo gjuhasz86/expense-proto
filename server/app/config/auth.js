3d0fb45ee1b14a1fc6821e563ce3767b3329f297
Gabor Juhasz <jaysicks.reg@gmail.com>
2016-11-19 23:24:08 +0100

    changed to https callback url

--- server/app/config/auth.js
+++ server/app/config/auth.js
@@ -4,6 +4,6 @@ var conf = {};
 
 conf.clientId = env.EXPENSE_GOOGLE_CLIENT_ID;
 conf.clientSecret = env.EXPENSE_GOOGLE_CLIENT_SECRET;
-conf.callbackURL = "http://" + env.EXPENSE_APP_DNS + "/auth/google/callback"
+conf.callbackURL = "https://" + env.EXPENSE_APP_DNS + "/auth/google/callback"
 
 module.exports = conf;
