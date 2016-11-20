6b58ac00a4c673e47ada44e0b0db7c43b580d62f
Gabor Juhasz <jaysicks.reg@gmail.com>
2016-11-20 12:06:31 +0100

    made redirect protocol configurable

--- server/app/config/auth.js
+++ server/app/config/auth.js
@@ -4,6 +4,6 @@ var conf = {};
 
 conf.clientId = env.EXPENSE_GOOGLE_CLIENT_ID;
 conf.clientSecret = env.EXPENSE_GOOGLE_CLIENT_SECRET;
-conf.callbackURL = "https://" + env.EXPENSE_APP_DNS + "/auth/google/callback"
+conf.callbackURL = env.EXPENSE_APP_REDIRECT_PROTOCOL + "://" + env.EXPENSE_APP_DNS + "/auth/google/callback"
 
 module.exports = conf;
