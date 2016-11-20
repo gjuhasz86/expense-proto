856a238937e9b29f049e21388cca8c13ee2b3b60
Gabor Juhasz <jaysicks.reg@gmail.com>
2016-11-20 13:13:36 +0100

    generalized model relay service

--- public/app/account/account-model.component.ts
+++ public/app/account/account-model.component.ts
@@ -1,62 +1,14 @@
 import {Injectable, Component, Input, Output, OnInit} from "@angular/core";
 import {AccountReqService} from "./account-req.service";
-import {BehaviorSubject} from "rxjs/BehaviorSubject";
 import {Account} from "./account";
-import {ReplaySubject} from "rxjs/ReplaySubject";
-import {Observable} from "rxjs/Observable";
+import {CommonModelRelayService} from "../common/common-model-relay.service";
+
 
 @Injectable()
-export class AccountModelRelayService {
-
-    readonly accountChange = new BehaviorSubject<Account[]>([]);
-
-    private readonly refresh$ = new ReplaySubject<boolean>();
-    private readonly save$ = new ReplaySubject<Account>();
-    private readonly remove$ = new ReplaySubject<string>();
-
-    constructor(private _svc: AccountReqService) {
-        this.refresh$.debounceTime(200)
-            .subscribe(x => this.doRefresh());
-        this.save$
-            .subscribe(a => this.doSave(a));
-        this.remove$
-            .subscribe(a => this.doRemove(a));
+export class AccountModelRelayService extends CommonModelRelayService<Account> {
+    constructor(_svc: AccountReqService) {
+        super(_svc);
     }
-
-    refresh() {
-        this.refresh$.next(true);
-    }
-
-    save(a: Account) {
-        this.save$.next(a);
-    }
-
-    remove(acc: Account) {
-        this.remove$.next(acc.id());
-    }
-
-    removeId(id: string) {
-        this.remove$.next(id);
-    }
-
-
-    private doRefresh(): void {
-        this._svc.list()
-            .subscribe(a => this.accountChange.next(a));
-    }
-
-    private doSave(a: Account): void {
-        this._svc.save(a)
-            .catch(e => Observable.of(null))
-            .subscribe(x => this.refresh());
-    }
-
-    private doRemove(id: string): void {
-        this._svc.remove(id)
-            .catch(e => Observable.of(null))
-            .subscribe(x => this.refresh());
-    }
-
 }
 
 @Component({
@@ -65,7 +17,7 @@ export class AccountModelRelayService {
 })
 export class AccountModelComponent implements OnInit {
     @Input() accounts: Account[];
-    @Output() accountsChange = this._relay.accountChange;
+    @Output() accountsChange = this._relay.onChange;
 
     constructor(private _relay: AccountModelRelayService) {
         this.accountsChange.subscribe(a => (this.accounts = a));
