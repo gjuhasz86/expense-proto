520c8723c6ac33b6029095f660c2714b32834e00
Gabor Juhasz <jaysicks.reg@gmail.com>
2016-11-22 09:15:14 +0100

    implemented paging for transactions

--- public/app/common/common-model-relay.service.ts
+++ public/app/common/common-model-relay.service.ts
@@ -3,27 +3,34 @@ import {BehaviorSubject} from "rxjs/BehaviorSubject";
 import {ReplaySubject} from "rxjs/ReplaySubject";
 import {Observable} from "rxjs/Observable";
 import "rxjs/add/observable/of"
+import "rxjs/add/observable/combineLatest"
 import 'rxjs/add/operator/debounceTime'
 import 'rxjs/add/operator/catch';
 
+export interface Filter {
+    page: number,
+    limit: number
+}
 export abstract class CommonModelRelayService<T> {
 
     readonly changed = new BehaviorSubject<T[]>([]);
 
+    protected readonly filter$ = new BehaviorSubject<Filter>(null);
     protected readonly refresh$ = new ReplaySubject<void>(1);
     protected readonly save$ = new ReplaySubject<T>(1);
     protected readonly update$ = new ReplaySubject<T>(1);
     protected readonly remove$ = new ReplaySubject<string>(1);
 
     constructor(protected _svc: CrudReqService<T>) {
-        this.refresh$.debounceTime(200)
-            .subscribe(x => this.onRefresh());
         this.save$
             .subscribe(a => this.onSave(a));
         this.update$
             .subscribe(a => this.onUpdate(a));
         this.remove$
             .subscribe(a => this.onRemove(a));
+        Observable.combineLatest(this.refresh$, this.filter$)
+                  .debounceTime(200)
+                  .subscribe(x => this.onRefresh(x[1]));
     }
 
     refresh() { this.refresh$.next(null); }
@@ -34,10 +41,17 @@ export abstract class CommonModelRelayService<T> {
 
     removeId(id: string) { this.remove$.next(id); }
 
+    filter(filt: Filter) { this.filter$.next(filt); }
 
-    private onRefresh(): void {
-        this._svc.list()
-            .subscribe(ts => this.changed.next(ts));
+    private onRefresh(filt: Filter): void {
+        console.log("onRefresh" + JSON.stringify(filt));
+        if (!filt) {
+            this._svc.list()
+                .subscribe(ts => this.changed.next(ts));
+        } else {
+            this._svc.search(filt.page, filt.limit)
+                .subscribe(ts => this.changed.next(ts));
+        }
     }
 
     private onSave(t: T): void {
--- public/app/transaction/transaction-list.component.ts
+++ public/app/transaction/transaction-list.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
-import {ActionRelayService} from "../common/action-relay-component";
 import {TransactionModelRelayService} from "./transaction-relay.component";
+import {Filter} from "../common/common-model-relay.service";
 
 @Component({
     selector: 'transaction-list',
     templateUrl: 'app/transaction/transaction-list.component.html'
 })
 export class TransactionListComponent {
+    private filter: Filter = {page: 1, limit: 2};
 
     constructor(private relay: TransactionModelRelayService) {}
 
